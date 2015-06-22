39c5b36c7d5284686e5aee65682e979b8e44960e
aneeshmg <aneesh.mg2007@gmail.com>
2015-06-22 21:42:17 +0530

    fetching images done

--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -2,8 +2,16 @@
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
+var sources = [
+  'http://lorempixel.com/480/640/',
+  'http://thecatapi.com/api/images/get?&type=jpg&size=med'
+];
+// More sources can be added
+
 function onDeviceReady() {
 
-  alert("Device ready!");
+  var imageUrl = sources[Math.ceil(Math.random() * 10) % sources.length];
+  
+  $('#board').append('<img src="' + imageUrl + '"></img>');
 
 }
