4c43d4009a87c87259f811d73aa8d6a5bbc3c2cc
aneeshmg <aneesh.mg2007@gmail.com>
2015-06-23 01:14:08 +0530

    using a better structure for retreving now and using canvas

--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -10,8 +10,29 @@ var sources = [
 
 function onDeviceReady() {
 
+  LoadImage();
+  
+  $('#newImage').click(function() {
+
+    var canvas = document.getElementById('board');
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    LoadImage();
+  });
+
+}
+
+function LoadImage() {
+
   var imageUrl = sources[Math.ceil(Math.random() * 10) % sources.length];
   
-  $('#board').append('<img src="' + imageUrl + '"></img>');
-
+  var ctx = $("canvas")[0].getContext("2d"),
+      img = new Image();
+    
+  img.onload = function(){
+    ctx.drawImage(img, 0, 0);
+  };
+    
+  img.src = imageUrl;
+  
 }
